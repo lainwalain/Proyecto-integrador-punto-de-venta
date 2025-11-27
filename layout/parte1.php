1e401f42ce17af5d974f39d3cbe02d25d8d58146
cesar <cesarharo9981@gmail.com>
2025-11-27 00:25:04 -0600

    al ingresar al sistema adminitrador muestre tu inicial

--- layout/parte1.php
+++ layout/parte1.php
@@ -62,15 +62,17 @@
 
         <!-- Sidebar -->
         <div class="sidebar">
-            <!-- Sidebar user panel (optional) -->
-            <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-                <div class="image">
-                    <img src="<?php echo $URL;?>/public/templeates/AdminLTE-3.2.0/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
-                </div>
-                <div class="info">
-                    <a href="#" class="d-block"><?php echo $nombres_sesion;?></a>
-                </div>
-            </div>
+           <!-- Sidebar user panel (optional) -->
+<div class="user-panel mt-3 pb-3 mb-3 d-flex">
+    <div class="image">
+        <div class="img-circle elevation-2 bg-info d-flex align-items-center justify-content-center" style="width: 38px; height: 38px;">
+            <span class="text-white font-weight-bold"><?php echo substr($nombres_sesion, 0, 1); ?></span>
+        </div>
+    </div>
+    <div class="info">
+        <a href="#" class="d-block"><?php echo $nombres_sesion;?></a>
+    </div>
+</div>
 
 
             <!-- Sidebar Menu -->
